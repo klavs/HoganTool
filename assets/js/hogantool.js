f3db6b109d11fe389352c0c40f958f062d2bdaf1
Klāvs Priedītis <klavs.pr@gmail.com>
2013-09-19 21:14:08 +0300

    minor update to editors

--- assets/js/hogantool.js
+++ assets/js/hogantool.js
@@ -3,16 +3,19 @@
     hashEditor.getSession().setUseWorker(false);
     hashEditor.setTheme("ace/theme/textmate");
     hashEditor.getSession().setMode("ace/mode/javascript");
+    hashEditor.setShowPrintMargin(false);
     
     var templateEditor = ace.edit("template");
     templateEditor.getSession().setUseWorker(false);
     templateEditor.setTheme("ace/theme/textmate");
-    templateEditor.getSession().setMode("ace/mode/javascript");
+    templateEditor.getSession().setMode("ace/mode/xml");
+    templateEditor.setShowPrintMargin(false);
     
     var resultEditor = ace.edit("result");
     resultEditor.getSession().setUseWorker(false);
     resultEditor.setTheme("ace/theme/textmate");
-    resultEditor.getSession().setMode("ace/mode/javascript");
+    resultEditor.getSession().setMode("ace/mode/xml");
+    resultEditor.setReadOnly(true);
 
     function generate(){
         var hash = JSON.parse(hashEditor.getValue());
