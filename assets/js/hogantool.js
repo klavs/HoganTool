2bd95c9168b73d8b11dd70eb81c65acfc1c9393d
Klāvs Priedītis <klavs.pr@gmail.com>
2013-09-19 16:48:01 +0300

    organizational changes

---
+++ assets/js/hogantool.js
@@ -0,0 +1,6 @@
+﻿    function generate(){
+        var hash = JSON.parse($("textarea#hash").val());
+        var template = Hogan.compile($("textarea#template").val());;
+        var result = template.render(hash)
+        $("textarea#result").val(result);
+    }
